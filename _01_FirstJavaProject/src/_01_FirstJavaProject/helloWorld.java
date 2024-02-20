05c26167c7e32a819b7f206ab0c6b4c30c026993
jidefine <youjio2000@gmail.com>
2024-02-20 10:24:50 +0900

    HelloEveryOne 출력

--- _01_FirstJavaProject/src/_01_FirstJavaProject/helloWorld.java
+++ _01_FirstJavaProject/src/_01_FirstJavaProject/helloWorld.java
@@ -4,5 +4,6 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("HelloWorld");
 		System.out.println("HelloJava");
+		System.out.println("HelloEveryOne");
 	}
 }
